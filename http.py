bc184bcd6af44e23ff00d03890ed8ee852d38b09
Wing Gao <wing.gao@live.com>
2015-09-19 01:22:50 +0800

    支持str类型

--- http.py
+++ http.py
@@ -4,4 +4,8 @@ import json
 
 class JSONResponse(HttpResponse):
     def __init__(self, obj):
-        super(JSONResponse, self).__init__(json.dumps(obj), content_type="application/json")
+        if isinstance(obj, dict):
+            _json_str = json.dumps(obj)
+        else:
+            _json_str = obj
+        super(JSONResponse, self).__init__(_json_str, content_type="application/json")
