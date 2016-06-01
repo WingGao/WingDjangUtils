55121f679c168050bcc25c735ed76528fcb6be9b
Wing Gao <wing.gao@live.com>
2016-06-01 09:53:39 +0800

    添加获取ip

--- tests.py
+++ tests.py
@@ -1,13 +1,16 @@
 __author__ = 'wing'
 import http
 import os
+import unittest
+import web
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-def test_download():
-    print http.download('http://omgmkt.qq.com/got/xz', os.path.join(BASE_DIR, 'temp', 't1', 't2', 'a'), create_dirs=True)
+class MyTest(unittest.TestCase):
+    def test_ip(self):
+        print(web.my_ip())
 
-
-if __name__ == '__main__':
-    test_download()
+    def test_download(self):
+        print http.download('http://omgmkt.qq.com/got/xz', os.path.join(BASE_DIR, 'temp', 't1', 't2', 'a'),
+                            create_dirs=True)
--- web.py
+++ web.py
@@ -1,5 +1,7 @@
 from urlparse import urljoin
 import HTMLParser
+import requests
+import re
 
 __author__ = 'wing'
 html_parser = HTMLParser.HTMLParser()
@@ -7,3 +9,9 @@ html_parser = HTMLParser.HTMLParser()
 
 def full_url(root, url):
     return html_parser.unescape(urljoin(root, url))
+
+
+def my_ip():
+    r = requests.get('http://1212.ip138.com/ic.asp')
+    ip = re.findall('\d+\.\d+\.\d+\.\d+', r.text)
+    return ip[0]
