2602d0dbac0e47e699e7cedd90917571b18d64c5
Wing Gao <wing.gao@live.com>
2016-06-11 00:46:05 +0800

    添加获取ip 添加ip验证

--- django/http.py
+++ django/http.py
@@ -1,13 +1,16 @@
 # coding=utf-8
-from django.http import HttpResponse
+from functools import wraps
+from django.http import HttpResponse, HttpResponseNotAllowed
 import json
+from django.utils.decorators import available_attrs
 from ..strings import get_int
+import os
 
 
 class JSONResponse(HttpResponse):
-    def __init__(self, obj):
+    def __init__(self, obj, sort_keys=False):
         if isinstance(obj, dict):
-            _json_str = json.dumps(obj)
+            _json_str = json.dumps(obj, sort_keys=sort_keys)
         else:
             _json_str = obj
         super(JSONResponse, self).__init__(_json_str, content_type="application/json")
@@ -40,3 +43,44 @@ class Params(object):
 
     def get_param_int(self, name, defv=None):
         return self.get_param(name, defv=defv, deft=Params.PARAMS_TYPE_INT)
+
+
+def get_ip(request, all_ips=False):
+    meta_keys = ['HTTP_X_FORWARDED_FOR', 'X_FORWARDED_FOR']
+    for key in meta_keys:
+        value = request.META.get(key, request.META.get(key.replace('_', '-'), '')).strip()
+        if value:
+            ips = [ip.strip().lower() for ip in value.split(',')]
+            count = len(ips)
+            if count > 1:
+                if all_ips:
+                    return ips
+                return ips[0]
+    return request.META.get('REMOTE_ADDR')
+
+
+# 装饰器
+def require_IP(ip):
+    def _require_IP(func):
+        @wraps(func, assigned=available_attrs(func))
+        def inner(request, *args, **kwargs):
+            if get_ip(request) in [ip, '127.0.0.1']:
+                return func(request, *args, **kwargs)
+            else:
+                return HttpResponseNotAllowed(['IP'])
+
+        return inner
+
+    return _require_IP
+
+
+def clear_request_meta(request):
+    """获得清爽的request.META
+    :param request:
+    :return:
+    """
+    d = {}
+    for k, v in request.META.items():
+        if k not in os.environ and not k.startswith('wsgi.'):
+            d[k] = v
+    return d
