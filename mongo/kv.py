171bd3e927dfb8d8f4d985a6625be3ef9ee864e2
Wing Gao <wing.gao@live.com>
2018-11-25 03:41:35 +0800

    添加kv

--- mongo/kv.py
+++ mongo/kv.py
@@ -7,6 +7,17 @@ class KVItem(ObjectBase):
         self.value = None
         super().__init__('kv', **kwargs)
 
+    def _pk_name(self):
+        return 'key', str
+
+    @classmethod
+    def exist(cls, key):
+        return KVItem().find_one({'key': key})
+
+    @classmethod
+    def set(cls, key, value):
+        return KVItem(key=key, value=value).save()
+
 
 def kv_index():
     KVItem().get_collection_inst().create_index('key', unique=True)
